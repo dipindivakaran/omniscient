b705b712f6b2d7cb6794fcd81cd84f916b76e135
dipindp85 <shijup@qburst.com>
2013-05-22 17:07:33 +0530

    Initial commit for app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omniscent::Application.config.secret_token = '6a140914b8d672722f4e5b9bf824d3add7e9fd2f9eaa07531924610147f61013fef25dceddd9da94535656a45a72f2fe14e5956b2d5f9b99b3d89de9d51437d5'
